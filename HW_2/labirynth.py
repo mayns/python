148326dd565efdb976e5d3db416b3a6ab64072e1
mayns <oksgorobets@gmail.com>
2014-10-28 00:45:30 +0300

    бугагашенька ^^

--- HW_2/labirynth.py
+++ HW_2/labirynth.py
@@ -42,26 +42,29 @@ way_out = {}
 
 
 def findaway(current_node):
-    if current_node == final_node:
-        return 1
-    next_nodes = nodes[current_node].get(u'children', [])
-    for node in next_nodes:
-        if (node not in nodes) or nodes[node][u'visited']:
-            continue
+    try:
+        if current_node == final_node:
+            return 1
+        next_nodes = nodes[current_node].get(u'children', [])
+        for node in next_nodes:
+            if (node not in nodes) or nodes[node][u'visited']:
+                continue
+            else:
+                next_node = node
+                break
         else:
-            next_node = node
-            break
-    else:
-        next_node = 0
-    if next_node:
-        nodes[next_node][u'parent'] = current_node
-        nodes[current_node][u'visited'] = 1
-        return findaway(next_node)
-    else:
-        next_node = nodes[current_node].get(u'parent', u'')
-        nodes.pop(current_node)
-        if not next_node:
-            return 0
-        return findaway(next_node)
+            next_node = 0
+        if next_node:
+            nodes[next_node][u'parent'] = current_node
+            nodes[current_node][u'visited'] = 1
+            return findaway(next_node)
+        else:
+            next_node = nodes[current_node].get(u'parent', u'')
+            nodes.pop(current_node)
+            if not next_node:
+                return 0
+            return findaway(next_node)
+    except Exception, ex:
+        return u'NO'
 
 print u'NO' if (start_node not in nodes) or (final_node not in nodes) or not findaway(start_node) else u'YES'
