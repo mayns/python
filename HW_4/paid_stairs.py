b73a61bd9695460f464ba23b828ff04b9be9efcc
mayns <oksgorobets@gmail.com>
2014-11-05 23:28:08 +0300

    stairs

--- HW_4/paid_stairs.py
+++ HW_4/paid_stairs.py
@@ -36,9 +36,7 @@ def paid_stairs(costs):
 
     return costs[-1] + min([i.pop() for i in mins.values()])
 
-if len(costs) < step:
+if len(costs) <= step:
     print costs[-1]
-elif len(costs) == step:
-    print costs[-1] + min(costs)
 else:
     print paid_stairs(costs[:])
