9b677eda880b8f9005056a50bac3e663be0e27a8
mayns <oksgorobets@gmail.com>
2014-11-05 07:59:08 +0300

    stairs

--- HW_4/paid_stairs.py
+++ HW_4/paid_stairs.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'mayns'
+
+costs = raw_input()
+step = int(raw_input())
+costs = map(int, costs.split(u','))
+k = len(costs)
+amin = []
+
+
+def min_pay(ak, klist):
+
+    global amin
+
+    if 1 < len(klist) <= step:
+        return min(klist) + ak
+
+    for i in xrange(1, step):
+        amin.append(ak + min_pay(klist[-i], klist[:-i]))
+    print amin
+    return ak + min(amin)
+
+print min_pay(costs[-1], costs[:-1])
