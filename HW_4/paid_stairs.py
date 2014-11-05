ee2fa1966001e1452aeadc841f8d59f344d40051
mayns <oksgorobets@gmail.com>
2014-11-05 21:49:25 +0300

    stairs

--- HW_4/paid_stairs.py
+++ HW_4/paid_stairs.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 __author__ = 'mayns'
 
 costs = raw_input()
 step = int(raw_input())
 costs = map(int, costs.split(u','))
 k = len(costs)
-amin = []
+
+mins = defaultdict(list)
 
 
-def min_pay(ak, klist):
+def paid_stairs(costs):
 
-    global amin
+    if not costs:
+        return 0
+    if len(costs) == 1:
+        return costs[0]
 
-    if 1 < len(klist) <= step:
-        return min(klist) + ak
+    for s in xrange(1, step+1):
+        mins[s].append(paid_stairs(costs[:-s]))
 
-    for i in xrange(1, step):
-        amin.append(ak + min_pay(klist[-i], klist[:-i]))
-    print amin
-    return ak + min(amin)
+    return costs[-1] + min([i.pop() for i in mins.values()])
 
-print min_pay(costs[-1], costs[:-1])
+print paid_stairs(costs[:])
