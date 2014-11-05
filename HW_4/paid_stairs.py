380bc2f9923fd16425fbe143a2328d7022c9146b
mayns <oksgorobets@gmail.com>
2014-11-06 00:49:28 +0300

    stairs

--- HW_4/paid_stairs.py
+++ HW_4/paid_stairs.py
@@ -8,7 +8,6 @@ __author__ = 'mayns'
 costs = raw_input()
 step = int(raw_input())
 costs = map(int, costs.split(u','))
-k = len(costs)
 
 mins = defaultdict(list)
 
@@ -30,13 +29,19 @@ def paid_stairs(costs):
         return 0
     if len(costs) == 1:
         return costs[0]
+    # try:
+    # if step > 5:
+    #     mins.clear()
+    for s in xrange(step):
+        mins[s].append(paid_stairs(costs[:-s-1]))
+    # except MemoryError:
+    #     return 0
+    # else:
+    m = [i.pop() for i in mins.values() if i]
 
-    for s in xrange(1, step+1):
-        mins[s].append(paid_stairs(costs[:-s]))
-
-    return costs[-1] + min([i.pop() for i in mins.values()])
+    return costs[-1] + min(m)
 
 if len(costs) <= step:
     print costs[-1]
 else:
-    print paid_stairs(costs[:])
+    print paid_stairs(costs)
