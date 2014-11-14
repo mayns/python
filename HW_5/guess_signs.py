cffc61a26c2ae16a2aa2b3259478906529558ca6
mayns <oksgorobets@gmail.com>
2014-11-14 18:39:01 +0300

    automaton

--- HW_5/guess_signs.py
+++ HW_5/guess_signs.py
@@ -16,7 +16,28 @@ numbers = raw_input()
 
 numbers = numbers.split(u' ')
 
+complexity = len(numbers)
+
 signs = [u'+', u'-']
 
 # 123 234 345 12
 
+from itertools import product
+
+combs = list(product(signs, repeat=complexity-2))
+
+enough = 0
+
+for comb in combs:
+    for i, number in enumerate(numbers[:-1]):
+        c = list(comb[:])
+        c.insert(i, u'==')
+        equation = u''.join(reduce(lambda x, y: x + y, zip(numbers, c))) + numbers[-1]
+        if eval(equation):
+            print u'YES'
+            enough = 1
+            break
+    if enough:
+        break
+else:
+    print u'NO'
--- HW_5/max_subst.py
+++ HW_5/max_subst.py
@@ -5,7 +5,7 @@ __author__ = 'oks'
 r = raw_input()
 
 a = [r.find(v, i+1) - i for i, v in enumerate(r)]
-
+print len(a)
 cur_max = 0
 max_ind = 0
 tester = a[:]
@@ -16,22 +16,22 @@ while tester[:-1]:
     c = tester.pop(0)
 
     if (c > 0) and (tester[0] >= c) and (c > cur_max):
-        print u'(c > 0) and (tester[0] > c) and (c > cur_max): ', c
+        # print u'(c > 0) and (tester[0] > c) and (c > cur_max): ', c
         cur_max = c
         max_ind = len(tester)
     if (c > 0) and (tester[0] < 0):
         pass
     if (c < 0) and (tester[0] > 0) and (c + counter > cur_max):
-        print u'(c < 0) and (tester[0] > 0) and (c + counter > cur_max)', c, tester[0]
+        # print u'(c < 0) and (tester[0] > 0) and (c + counter > cur_max)', c, tester[0]
         cur_max = c + counter
         max_ind = len(tester)
     if (c < 0) and (tester[0] > 0):
         counter = 1
     if (c < 0) and (tester[0] < 0):
-        print u'(c < 0) and (tester[0] < 0): ', c, tester[0], counter
+        # print u'(c < 0) and (tester[0] < 0): ', c, tester[0], counter
         counter += 1
     else:
-        print u'ELSE'
+        # print u'ELSE'
         continue
 
 cur_max = counter if counter > cur_max else cur_max
