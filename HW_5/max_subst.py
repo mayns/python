081ee4f3d471a856d8332de789378b48a4ec48ef
mayns <oksgorobets@gmail.com>
2014-11-15 16:05:02 +0300

    max substring

--- HW_5/max_subst.py
+++ HW_5/max_subst.py
@@ -2,37 +2,18 @@
 
 __author__ = 'oks'
 
-r = raw_input()
+s = raw_input()
 
-a = [r.find(v, i+1) - i for i, v in enumerate(r)]
-print len(a)
-cur_max = 0
-max_ind = 0
-tester = a[:]
+max_substr = u''
+frame = []
 
-counter = 1
-
-while tester[:-1]:
-    c = tester.pop(0)
-
-    if (c > 0) and (tester[0] >= c) and (c > cur_max):
-        # print u'(c > 0) and (tester[0] > c) and (c > cur_max): ', c
-        cur_max = c
-        max_ind = len(tester)
-    if (c > 0) and (tester[0] < 0):
-        pass
-    if (c < 0) and (tester[0] > 0) and (c + counter > cur_max):
-        # print u'(c < 0) and (tester[0] > 0) and (c + counter > cur_max)', c, tester[0]
-        cur_max = c + counter
-        max_ind = len(tester)
-    if (c < 0) and (tester[0] > 0):
-        counter = 1
-    if (c < 0) and (tester[0] < 0):
-        # print u'(c < 0) and (tester[0] < 0): ', c, tester[0], counter
-        counter += 1
-    else:
-        # print u'ELSE'
+for c in s:
+    if c not in frame:
+        frame.append(c)
         continue
+    if len(frame) > len(max_substr):
+        max_substr = u''.join(frame)
+    frame = frame[frame.index(c)+1:]
+    frame.append(c)
 
-cur_max = counter if counter > cur_max else cur_max
-print r[max_ind:max_ind+cur_max]
+print max_substr if max_substr else u''.join(frame)
