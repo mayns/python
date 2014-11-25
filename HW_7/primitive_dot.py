39c6100a3c6542a81520939cd15cc006f6cba733
mayns <oksgorobets@gmail.com>
2014-11-25 03:14:54 +0300

    777

--- HW_7/primitive_dot.py
+++ HW_7/primitive_dot.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-
+from itertools import imap
 __author__ = 'oks'
 
 
 class Dot(object):
     def __init__(self, *args):
-        self.point = args
+        self.point = list(args)
 
     def __str__(self):
-        return u','.join(self.point)
+        return u','.join(map(str, self.point))
 
     def distance(self, other):
         if len(self.point) != len(other.point):
             raise ValueError
-        return
+        return sum(imap(lambda x, y: (x - y)**2, self.point, other.point))**0.5
 
     def middle(self, other):
         if len(self.point) != len(other.point):
             raise ValueError
-        return
+        return Dot(*list(imap(lambda x, y: (x + y) / 2.0, self.point, other.point)))
