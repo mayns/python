208e302bfb9905eb96c5a30d0792b831011c0000
mayns <oksgorobets@gmail.com>
2014-12-04 01:12:24 +0300

    88

--- HW_8/selfish_tuple.py
+++ HW_8/selfish_tuple.py
@@ -11,22 +11,36 @@ __author__ = 'oks'
 
 
 class Meta(type):
-    pass
+
+    def __new__(cls, name, bases, dct):
+
+        dct.update()
+        for k, v in dct.iteritems():
+            print k, v
+            if hasattr(name, k):
+                try:
+                    setattr(name, k, v)
+                except AttributeError:
+                    continue
+        return super(Meta, cls).__new__(cls, name, bases, dct)
 
 
 class MTuple(tuple):
+    __metaclass__ = Meta
 
     def __init__(self, seq=()):
-        super(MTuple, self).__init__(seq)
-        self.seq = seq
+        super(MTuple, self).__init__()
+        self._seq = seq
 
     def __neg__(self):
-        return MTuple(self.seq[::-1])
-
+        return self.__class__(self._seq[::-1])
 
 
 c = MTuple(range(10))
+print -c
+print type(-c)
 print c[2:6]
+print type(c[2:6])
 # print -(-c[2:6]+c[-1:2:-2])
 # print -c[7:9]+("Bdyshch", "Bdyshch")
 # print {-c[3:5]:"QQ"}
--- HW_8/trigon_pea.py
+++ HW_8/trigon_pea.py
@@ -11,8 +11,8 @@ class Trigon(object):
         self.c = c
 
     def square(self):
-        h = 0
-        return self.c + 0.5 * h
+        p = self.perimeter() / 2.0
+        return (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
 
     def perimeter(self):
         return self.a + self.b + self.c
@@ -25,15 +25,34 @@ class Pea(object):
         self.b = b
         self.c = c
 
+    def calc_rad(self):
+        p = (self.a + self.b + self.c) / 2.0
+        heron = (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
+        return (self.a * self.b * self.c) / (4 * heron)
+
     def square(self):
-        pass
+        r = self.calc_rad()
+        return math.pi * r ** 2
 
     def perimeter(self):
-        r = 0
+        r = self.calc_rad()
         return 2 * math.pi * r
 
 
 class TrigonPea(Trigon, Pea):
+    def __init__(self, a, b, c):
+        super(TrigonPea, self).__init__(a, b, c)
 
     def volume(self):
-        pass
+        perimeter = Trigon.perimeter(self)
+        square = Pea.square(self)
+        return perimeter * square
+
+# t = Trigon(3, 4, 5)
+# p = Pea(3, 4, 5)
+# z = TrigonPea(3, 4, 5)
+# print "{:.6f}".format(t.square())
+# print "{:.6f}".format(t.perimeter())
+# print "{:.6f}".format(p.square())
+# print "{:.6f}".format(z.volume())
+# print "{:.6f}".format(z.square())
